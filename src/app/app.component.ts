dd263b3ac139003ac666762230fdc5293da6c7ef
Aaqib <aaqibkhan658@gmail.com>
2022-06-27 23:05:03 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component,OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  
+  ngOnInit(): void {
+    this.loadJsFile("../../assets/js/script.js");
+  }
+  public loadJsFile(url :string) {  
+    let node = document.createElement('script');  
+    node.src = url;  
+    node.type = 'text/javascript';  
+    document.getElementsByTagName('head')[0].appendChild(node);  
+  } 
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingItemComponent } from './shopping-list/shopping-item/shopping-item.component';
+import { OrderListComponent } from './order-list/order-list.component';
+import { OrderItemComponent } from './order-list/order-item/order-item.component';
+import { MyAccountComponent } from './my-account/my-account.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { FooterComponent } from './footer/footer.component';
+import { CartItemComponent } from './shopping-cart/cart-item/cart-item.component';
+import { ItemDetailsComponent } from './shopping-list/item-details/item-details.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingItemComponent,
+    OrderListComponent,
+    OrderItemComponent,
+    MyAccountComponent,
+    ShoppingCartComponent,
+    FooterComponent,
+    CartItemComponent,
+    ItemDetailsComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
