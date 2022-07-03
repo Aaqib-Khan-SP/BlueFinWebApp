d07b2852846aa9d4e76378f67e84a41cc77f08a3
Aaqib <aaqibkhan658@gmail.com>
2022-07-04 00:45:19 +0530

    project setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyAccountComponent } from './my-account/my-account.component';
+import { OrderListComponent } from './order-list/order-list.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'cart', component : ShoppingCartComponent },
+  { path:'myaccount', component : MyAccountComponent },
+  { path:'orders', component : OrderListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
