2334e1855f577005d7a48efc97324f6141cd64f0
Aaqib <aaqibkhan658@gmail.com>
2022-08-05 20:23:55 +0530

    checkout component changes

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from 'src/shared/models';
+import { LocalStorageService } from 'src/shared/services/local-storage.service';
 
 @Component({
   selector: 'app-checkout',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
-
-  constructor() { }
+  cart :Item[];
+  total :number = 0;
+  addressButton :boolean = true;
+  constructor(private localStorageService : LocalStorageService) { }
 
   ngOnInit(): void {
+    this.cart = this.localStorageService.getCartItems();
+    this.cart.forEach(item => this.total = this.total + item.rate*item.quantity);
   }
 
 }
