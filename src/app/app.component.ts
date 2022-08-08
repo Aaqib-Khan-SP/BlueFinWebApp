625e74273072ef4e71cd99e89340e40347ae887c
Aaqib <aaqibkhan658@gmail.com>
2022-08-09 00:46:53 +0530

    pop up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,11 @@ import { Component,OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   
   ngOnInit(): void {
-    // this.loadJsFile("../../assets/js/script.js");
-    this.loadJsFile("../../assets/js/jquery.min.js");
-    this.loadJsFile("../../assets/js/bootstrap.bundle.min.js");
-    this.loadJsFile("../../assets/js/jquery.waypoints.min.js");
-    this.loadJsFile("../../assets/js/main.js");
+    // this.loadJsFile("../../assets/js/jquery.magnific-popup.min.js");
+    // this.loadJsFile("../../assets/js/jquery.min.js");
+    // this.loadJsFile("../../assets/js/bootstrap.bundle.min.js");
+    // this.loadJsFile("../../assets/js/jquery.waypoints.min.js");
+    // this.loadJsFile("../../assets/js/main.js");
   }
   public loadJsFile(url :string) {  
     let node = document.createElement('script');  
