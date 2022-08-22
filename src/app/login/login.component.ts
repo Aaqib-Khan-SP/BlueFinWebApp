4fb4d8b5ee7821560f1ea8ecb5d2a608a6e644d0
Aaqib <aaqibkhan658@gmail.com>
2022-08-22 22:10:24 +0530

    removing sample credentials

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -42,8 +42,8 @@ export class LoginComponent implements OnInit {
   private signInForm() {
     //password and phone number default due to issues in receiving otp internationally, only for demo purpose.
     this.signInFormGroup = new FormGroup({
-      phoneNumber: new FormControl('+919999999999', [Validators.required,Validators.minLength(13), Validators.maxLength(13)]),
-      password: new FormControl('weakpassword', Validators.required),
+      phoneNumber: new FormControl(null, [Validators.required,Validators.minLength(13), Validators.maxLength(13)]),
+      password: new FormControl(null, Validators.required),
     })
   }
 
